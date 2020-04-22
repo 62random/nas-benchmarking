572adf547510e964bf847f1f5a53b0efded006f6
51Megamind <alexrone.rone3@gmail.com>
2020-04-22 17:41:52 +0100

    Update scriptMPI.sh

--- scriptMPI.sh
+++ scriptMPI.sh
@@ -2,10 +2,10 @@
 #setup enviroment
 
 
-#PSB -N MPI_ESC_431
+#PSB -N MPI_ESC_641
 #PBS -l walltime=02:00:00
 #PBS -q mei
-#PBS -l nodes=2:r431:ppn=24
+#PBS -l nodes=2:r641:ppn=32
 
 #AUX XmCW9LJQ
 
@@ -32,7 +32,6 @@ for c in "${classes[@]}"
 do
     for t in "${tests[@]}"
     do 
-            echo "${t}-mz ${c} 1" >> ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/config/suite.def
             echo "${t}-mz ${c} 2" >> ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/config/suite.def
             echo "${t}-mz ${c} 4" >> ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/config/suite.def
     done
@@ -42,8 +41,8 @@ done
 # run for mri and gnu compiler
 # Load required modules 
 module purge
-module load gnu/openmpi_mx/1.8.4 
 module load gcc/4.9.0
+module load gnu/openmpi_mx/1.8.4 
 
 # Copy make.def to build path
 cp make.def ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/config/make.def
@@ -58,12 +57,10 @@ for c in "${classes[@]}"
 do
     for t in "${tests[@]}"
     do 
-            export OMP_NUM_THREADS=24
-            mpirun -np 1 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.1 > ${result_folder}/${t}_${c}_gnu_mri_1
-            export OMP_NUM_THREADS=24
-            mpirun -np 2 --map-by ppr:1:node:PE=12 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.2 > ${result_folder}/${t}_${c}_gnu_mri_node
-            export OMP_NUM_THREADS=12
-            mpirun -np 4 --map-by ppr:1:socket:PE=6 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.4 > ${result_folder}/${t}_${c}_gnu_mri_socket
+            export OMP_NUM_THREADS=32
+            mpirun -np 2 --bind-to hwthread --map-by ppr:1:node:pe=32 -x OMP_NUM_THREADS -mca mtl mx -mca pml cm  ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.2 > ${result_folder}/${t}_${c}_gnu_mri_node
+            export OMP_NUM_THREADS=16
+            mpirun -np 4 --bind-to hwthread --map-by ppr:2:node:pe=16 -x OMP_NUM_THREADS -mca mtl mx -mca pml cm  ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.4 > ${result_folder}/${t}_${c}_gnu_mri_socket
     done
 done
 
@@ -86,40 +83,10 @@ for c in "${classes[@]}"
 do
     for t in "${tests[@]}"
     do 
-            export OMP_NUM_THREADS=24
-            mpirun -np 1 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.1 > ${result_folder}/${t}_${c}_intel_mri_1
-            export OMP_NUM_THREADS=24
-            mpirun -np 2 --map-by ppr:1:node:PE=12 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.2 > ${result_folder}/${t}_${c}_intel_mri_node
-            export OMP_NUM_THREADS=12
-            mpirun -np 4 --map-by ppr:1:socket:PE=6 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.4 > ${result_folder}/${t}_${c}_intel_mri_socket
-    done
-done
-
-#Re-run for intel-compiler gbe
-# Load required modules 
-module purge
-module load intel/2019
-module load intel/openmpi_eth/1.8.4
-
-# Copy make.def(intel) to build path
-cp make.def ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/config/make.def
-
-# Build benchmarks with config files
-cd ../NPB3.3.1-MZ/NPB3.3-MZ-MPI
-make suite
-
-# Run benchmarks and save results
-cd ../../MPI
-for c in "${classes[@]}"
-do
-    for t in "${tests[@]}"
-    do 
-            export OMP_NUM_THREADS=24
-            mpirun -np 1 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.1 > ${result_folder}/${t}_${c}_intel_eth_1
-            export OMP_NUM_THREADS=24
-            mpirun -np 2 --map-by ppr:1:node:PE=12 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.2 > ${result_folder}/${t}_${c}_intel_eth_node
-            export OMP_NUM_THREADS=12
-            mpirun -np 4 --map-by ppr:1:socket:PE=6 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.4 > ${result_folder}/${t}_${c}_intel_eth_socket
+            export OMP_NUM_THREADS=32
+            mpirun -np 2 --bind-to hwthread --map-by ppr:1:node:pe=32 -x OMP_NUM_THREADS -mca mtl mx -mca pml cm  ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.2 > ${result_folder}/${t}_${c}_intel_mri_node
+            export OMP_NUM_THREADS=16
+            mpirun -np 4 --bind-to hwthread --map-by ppr:2:node:pe=16 -x OMP_NUM_THREADS -mca mtl mx -mca pml cm  ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.4 > ${result_folder}/${t}_${c}_intel_mri_socket
     done
 done
 
@@ -141,14 +108,10 @@ cd ../../MPI
 for c in "${classes[@]}"
 do
     for t in "${tests[@]}"
-    do 
-            export OMP_NUM_THREADS=24
-            mpirun -np 1 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.1 > ${result_folder}/${t}_${c}_gnu_eth_1
-            export OMP_NUM_THREADS=24
-            mpirun -np 2 --map-by ppr:1:node:PE=12 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.2 > ${result_folder}/${t}_${c}_gnu_eth_node
-            export OMP_NUM_THREADS=12
-            mpirun -np 4 --map-by ppr:1:socket:PE=6 -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.4 > ${result_folder}/${t}_${c}_gnu_eth_socket
+    do      
+            export OMP_NUM_THREADS=32
+            mpirun -np 2 --bind-to hwthread --map-by ppr:1:node:pe=32 -x OMP_NUM_THREADS -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.2 > ${result_folder}/${t}_${c}_gnu_eth_node
+            export OMP_NUM_THREADS=16
+            mpirun -np 4 --bind-to hwthread --map-by ppr:2:node:pe=16 -x OMP_NUM_THREADS -mca btl self,sm,tcp ../NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/${t}-mz.${c}.4 > ${result_folder}/${t}_${c}_gnu_eth_socket
     done
 done
-
-
